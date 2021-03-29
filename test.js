8be54e5bb499f5ed491a3297fc38de4bbe617c90
Aleks <aleksandra.slowik88@gmail.com>
2021-03-29 17:24:18 +0200

    ad form

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("heyyyy");
+/*console.log("heyyyy");
+
+const readlineSync = require("readline-sync");
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);*/
+
+let age = 23;
+let sisterAge = age + 11;
+age += 12;
+console.log(age);
